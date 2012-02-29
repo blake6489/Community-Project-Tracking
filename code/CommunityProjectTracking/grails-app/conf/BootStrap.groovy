1d9b40baf4002bffe8adad3667ca2d40edc3729d
Devin Jones <dcjones@alaska.edu>
2012-02-28 19:14:37 -0900

    Commiting before rename

--- code/CommunityProjectTracking/grails-app/conf/BootStrap.groovy
+++ code/CommunityProjectTracking/grails-app/conf/BootStrap.groovy
@@ -7,8 +7,8 @@ class BootStrap {
 	def init = { servletContext ->
 		//Create users
 		if(!User.count()) {
-			def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-			def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+			def adminRole = Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: 'ROLE_ADMIN').save(flush: true)
+			def userRole = Role.findByAuthority("ROLE_USER") ?: new Role(authority: 'ROLE_USER').save(flush: true)
 	
 			def admin = new User(username: 'admin', enabled: true, password: 'a').save(flush: true)
 			def user = new User(username: 'user', enabled: true, password: 'a').save(flush: true)
